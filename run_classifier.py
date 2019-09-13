ff84bc1c805ce69c70da0cb24f63bcac4c95942f
Bhavitvya Malik <bhavitvya.malik@gmail.com>
2019-09-13 10:41:15 +0530

    verified dataset

--- run_classifier.py
+++ run_classifier.py
@@ -605,12 +605,12 @@ def create_model(bert_config, is_training, input_ids, input_mask, segment_ids,
 
     logits = tf.matmul(output_layer, output_weights, transpose_b=True)
     logits = tf.nn.bias_add(logits, output_bias)
-    probabilities = tf.nn.softmax(logits, axis=-1)
-    log_probs = tf.nn.log_softmax(logits, axis=-1)
+    probabilities = tf.nn.softmax(logits, axis=-1)  #sigmoid
+    log_probs = tf.nn.log_softmax(logits, axis=-1)  #no need
 
     one_hot_labels = tf.one_hot(labels, depth=num_labels, dtype=tf.float32)
 
-    per_example_loss = -tf.reduce_sum(one_hot_labels * log_probs, axis=-1)
+    per_example_loss = -tf.reduce_sum(one_hot_labels * log_probs, axis=-1)  #mean squared
     loss = tf.reduce_mean(per_example_loss)
 
     return (loss, per_example_loss, logits, probabilities)
