8b72bd03dcb0c69fd531a222b6b57618add4b846
Bhavitvya Malik <bhavitvya.malik@gmail.com>
2019-09-15 13:52:30 +0530

    2nd method

--- run_regressional.py
+++ run_regressional.py
@@ -610,7 +610,7 @@ def create_model(bert_config, is_training, input_ids, input_mask, segment_ids,
     #probabilities = tf.nn.softmax(logits, axis=-1)  #sigmoid
     #log_probs = tf.nn.log_softmax(logits, axis=-1)  #no need        #Tensor("loss/LogSoftmax:0", shape=(16, 6), dtype=float32)
     #label_scores = tf.cast(labels, tf.float32)
-
+    tf.cast(labels, tf.float32)
     probabilities=logits
     log_probs=logits
     one_hot_labels=tf.one_hot(labels, depth=num_labels, dtype=tf.float32, on_value=labels, off_value=0.0,axis=-1)
