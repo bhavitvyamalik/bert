be0207aa7a0092062bb7f64d466c13e8bce031e9
Bhavitvya Malik <bhavitvya.malik@gmail.com>
2019-09-19 10:49:52 +0530

    last try for today (3)

--- run_regressional.py
+++ run_regressional.py
@@ -622,6 +622,7 @@ def main(_):
       config=run_config,
       train_batch_size=FLAGS.train_batch_size,
       eval_batch_size=FLAGS.eval_batch_size,
+      predict_batch_size=FLAGS.train_batch_size
       )
 
   if FLAGS.do_train:
@@ -683,7 +684,7 @@ def main(_):
 
     tf.logging.info("***** Running test *****")
     tf.logging.info("  Num examples = %d", len(test_examples))
-    tf.logging.info("  Batch size = %d", FLAGS.train_batch_size)
+    tf.logging.info("  Batch size = %d", FLAGS.predict_batch_size)
 
     # This tells the estimator to run through the entire set.
     test_steps = None
@@ -692,7 +693,7 @@ def main(_):
     if FLAGS.use_tpu:
       # Eval will be slightly WRONG on the TPU because it will truncate
       # the last batch.
-      test_steps = int(len(test_examples) / FLAGS.train_batch_size)
+      test_steps = int(len(test_examples) / FLAGS.predict_batch_size)
 
     test_drop_remainder = True if FLAGS.use_tpu else False
     test_input_fn = input_fn_builder(
